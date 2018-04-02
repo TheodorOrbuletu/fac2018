b98588792bf10f34e5e7a6e0cfac05bd0bf4e280
Theodor Daniel <theodordaniel4@gmail.com>
2018-04-02 19:28:40 +0300

    added semaphore

--- home1.py
+++ home1.py
@@ -4,32 +4,42 @@ import time
 
 mutex = Lock()
 produced = []
+maxSize = 5
+size = 0
 
 class threadProd(Thread):
 	def run(self):
 		while True:
-			mutex.acquire()
-			num = random.randint(1, 10)
+			#num = random.randint(1, 10)
 			global produced
-			produced.append(num)
-			print "produces", num
-			time.sleep(1)
-			print produced
-			mutex.release()
+			global size
+			if size < maxSize:
+				num = random.randint(1, 10)
+				mutex.acquire()
+				produced.append(num)
+				size = size + 1
+				mutex.release()
+				print "produces", num
+				print produced
+				time.sleep(0.6)
 
 class threadCons(Thread):
 	def run(self):
 		while True:
-			mutex.acquire()
 			global produced
+			global size
 			if not  produced:
-				mutex.wait()
-				time.sleep(random.random())
-			else:
-				aux = produced.pop()
-				print "removed", aux 
+				pass
+				print "waiting for producer"
 				time.sleep(0.5)
-			mutex.release()
+			else:
+				i = produced[0]
+				mutex.acquire()
+				aux = produced.remove(i)
+				size = size - 1
+				mutex.release()
+				print "removed", i 
+				time.sleep(0.8)
 
 threadProd().start()
 threadCons().start()
